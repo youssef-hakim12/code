ea47615cc37aa9143ab75de8f83bc0347caaa0e9
youssef assad <115468485+youssef-hakim12@users.noreply.github.com>
2025-03-10 09:47:20 +0200

    Add files via upload

---
+++ database/migrations/2025_03_08_054247_create_payments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string('bank');
+            $table->string('card_number');
+            $table->string('exp_month');
+            $table->string('exp_year');
+            $table->string('pin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
